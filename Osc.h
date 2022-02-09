f9950b22fb53c3b9de42bebe13bc7c6750b3d330
Alexis ZBIK <alexis.zbik@gmail.com>
2022-02-09 11:12:45 +0100

    add squares animation for the song "Water"

--- Osc.h
+++ Osc.h
@@ -6,6 +6,10 @@
 
 #define NO_VALUE 100
 
+#define START_NOTE 48
+#define VU_H 16
+#define VU_W 16
+
 class Osc : public Scene {
   
 public:
@@ -27,7 +31,48 @@ public:
     }
   }
 
+  virtual void midiNote(byte noteValue) override {
+
+    if (noteValue >= START_NOTE && noteValue <= (START_NOTE + 3))
+    {
+      squareMode = true;
+      squarePosition = noteValue - START_NOTE;
+      needRefresh = true;
+    } else {
+      if (squareMode) {
+        needsClear = true;
+      }
+      squareMode = false;
+      needRefresh = true;
+    }
+ 
+  }
+
   virtual void draw() override {
+
+    if (squareMode) {
+      if (xSqr >= 0) {
+        matrix.fillRect(xSqr, 0, VU_W, VU_H, COLOR(0,0,0));
+      }
+
+      byte colorIndex = random(1000) % palette->size;
+      color_t c = palette->colors[colorIndex];
+
+      xSqr = isOtherDisplay ? (((int)squarePosition - 2) * (int)VU_W) : (int)squarePosition * (int)VU_W;
+      
+      if (xSqr >= 0) {
+        matrix.fillRect(xSqr, 0, VU_W, VU_H, c);
+      }
+      
+      
+    } else {
+      if (needsClear) {
+        if (xSqr >= 0) {
+          matrix.fillRect(xSqr, 0, VU_W, VU_H, COLOR(0,0,0));
+        }
+        needsClear = false;
+      }
+      
       const byte w = displayW/2;
 
       for (byte i = 0; i < w; i++) {
@@ -53,6 +98,7 @@ public:
           matrix.drawPixel(i, buf[s], c);
         }
       }
+    }
   }
 
 private:
@@ -60,6 +106,12 @@ private:
   byte waveCount = 0;
   byte* buf;
   Palette* palette;
+
+  bool squareMode = false;
+  bool needsClear = false;
+  byte squarePosition = 0;
+
+  int xSqr = 0;
 };
 
 
